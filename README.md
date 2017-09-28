d826d69c3c069c5b8bd1c5cb6a94a9151c32b182
ZOZAKIEWICZ AARON <aj.zozakiewicz@gmail.com>
2017-09-27 21:09:33 -0500

    redux wiring

---
+++ README.md
@@ -0,0 +1,15 @@
+# React Native Workshop 4 Solution App
+
+## Getting Started
+1.) To run the project. Install the packages and run the project by executing the following in the project folder.
+
+```
+yarn 
+
+yarn start
+
+```
+
+2.) open xcode project
+
+3.) run emulator (play button)
--- __tests__/BackBar.js
+++ __tests__/BackBar.js
@@ -1,13 +1,15 @@
-/* global it */
+/* globals it expect */
 import 'react-native'
 import React from 'react'
-import Index from '../index.ios.js'
+import BackBar from '../components/BackBar'
 
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer'
 
 it('renders correctly', () => {
   const tree = renderer.create(
-    <Index />
+    <BackBar />
   )
+
+  expect(tree).toBeTruthy()
 })
